191976688616c397b55e5c75005d2c8081bfbc13
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-08-03 21:11:15 -0600

    finished draft of UI
    now readdy to make the api calls

--- src/mainDashView.js
+++ src/mainDashView.js
@@ -5,26 +5,36 @@ var currentButton = document.querySelector(".currentButton")
 var historyButton = document.querySelector(".historyButton")
 var loginPage = document.querySelector(".loginPage")
 var dashBoard = document.querySelector(".dashBoard")
-
-
+var BookingButton = document.querySelector(".BookingButton")
+var bookingPage = document.querySelector(".bookingPage")
 logOutButton.addEventListener('click', logOut);
-historyButton.addEventListener("click", currentToPastSwitch)
-currentButton.addEventListener("click", pastToCurrentSwitch)
-
+historyButton.addEventListener("click", PastSwitch)
+currentButton.addEventListener("click", CurrentSwitch)
+BookingButton.addEventListener("click",bookingSwitch)
 function logOut () {
   console.log('here')
   loginPage.classList.remove('hidden');
   dashBoard.classList.add('hidden');
+  bookingPage.classList.add("hidden")
 }
-function currentToPastSwitch ()  {
+function PastSwitch ()  {
   currentBookingsPage.classList.add("hidden")
   historyBookingsPage.classList.remove("hidden")
+  bookingPage.classList.add("hidden")
 
 }
-function pastToCurrentSwitch(){
+function CurrentSwitch() {
   historyBookingsPage.classList.add("hidden")
   currentBookingsPage.classList.remove("hidden")
+  bookingPage.classList.add("hidden")
 }
+function bookingSwitch() {
+  historyBookingsPage.classList.add("hidden")
+  currentBookingsPage.classList.add("hidden")
+  bookingPage.classList.remove("hidden")
+}
+
+
 
 
 
