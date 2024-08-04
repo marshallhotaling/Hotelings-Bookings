0303c5e69e1dad8f5627eec71074e29be2528af1
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-08-04 13:12:16 -0600

    made the user obj able to be used by other functions
    made backend for the user to track there id and name and implumentation of the id to the bookings and rooms datta set

--- src/loginView.js
+++ src/loginView.js
@@ -1,9 +1,12 @@
 import { customerData } from './calls/fetchCustomers.js';
-var user = {};
+
+export var user = {};
 var loginButon = document.querySelector(".enterUserButton");
 var loginPage = document.querySelector(".loginPage");
 var loginInput = document.querySelector(".userNInput")
 var dashBoard = document.querySelector(".dashBoard")
+var homePage = document.querySelector(".homePage")
+
 
 loginButon.addEventListener('click', showMainPage);
 
@@ -17,13 +20,17 @@ function checkForUsar(name) {
     customerData(),
   ])
     .then(data => {
-      const found = data.filter((x) => x.customers.some((y) => y.name === name));
+      const found = data[0].customers.filter((item) => {
+        return item.name === name
+      })
       if (found.length > 0) {
-        user = name
+        user = found[0]
         loginPage.classList.add('hidden');
         dashBoard.classList.remove('hidden');
+        homePage.classList.remove('hidden');
       }
     })
     .catch(err => console.log(err));
 }
 
+checkForUsar('aa')
--- src/mainDashView.js
+++ src/mainDashView.js
@@ -1,3 +1,9 @@
+import { bookingsData } from './calls/fetchBookings.js';
+import { roomsData } from './calls/fetchRooms.js';
+import {user} from "./loginView";
+
+
+
 var historyBookingsPage = document.querySelector(".historyBookingsPage")
 var currentBookingsPage = document.querySelector(".currentBookingsPage")
 var logOutButton = document.querySelector(".logOutButton")
@@ -7,34 +13,86 @@ var loginPage = document.querySelector(".loginPage")
 var dashBoard = document.querySelector(".dashBoard")
 var BookingButton = document.querySelector(".BookingButton")
 var bookingPage = document.querySelector(".bookingPage")
+var homeButton = document.querySelector(".homeButton")
+var homePage = document.querySelector(".homePage")
+var loginInput = document.querySelector(".userNInput")
+
+homeButton.addEventListener('click', homeSwitch)
 logOutButton.addEventListener('click', logOut);
 historyButton.addEventListener("click", PastSwitch)
 currentButton.addEventListener("click", CurrentSwitch)
-BookingButton.addEventListener("click",bookingSwitch)
+BookingButton.addEventListener("click", bookingSwitch)
+
+
+
 function logOut () {
-  console.log('here')
+  hideViews()
   loginPage.classList.remove('hidden');
   dashBoard.classList.add('hidden');
-  bookingPage.classList.add("hidden")
+  //make sure you add for pasword
+  loginInput.value = ""
 }
 function PastSwitch ()  {
-  currentBookingsPage.classList.add("hidden")
+  hideViews()
   historyBookingsPage.classList.remove("hidden")
-  bookingPage.classList.add("hidden")
-
 }
 function CurrentSwitch() {
-  historyBookingsPage.classList.add("hidden")
+  hideViews()
   currentBookingsPage.classList.remove("hidden")
-  bookingPage.classList.add("hidden")
 }
 function bookingSwitch() {
+  hideViews()
+  bookingPage.classList.remove("hidden")
+}
+function homeSwitch() {
+  hideViews()
+  homePage.classList.remove("hidden")
+  console.log("user", user)
+}
+function hideViews() {
   historyBookingsPage.classList.add("hidden")
   currentBookingsPage.classList.add("hidden")
-  bookingPage.classList.remove("hidden")
+  bookingPage.classList.add("hidden")
+  homePage.classList.add("hidden")
+
+}
+
+function checkForBookings(name) {
+  console.log("loginInput", name)
+  Promise.all([
+    bookingsData(),
+  ])
+    .then(data => {
+      console.log("here is bookings", data);
+      // const found = data.filter((x) => x.customers.some((y) => y.name === name));
+      // if (found.length > 0) {
+      //   user = name
+      //   loginPage.classList.add('hidden');
+      //   dashBoard.classList.remove('hidden');
+      // }
+    })
+    .catch(err => console.log(err));
+}
+
+
+function checkForRooms(name) {
+  console.log("loginInput", name)
+  Promise.all([
+    roomsData(),
+  ])
+    .then(data => {
+      console.log("here is rooms", data)
+      // const found = data.filter((x) => x.customers.some((y) => y.name === name));
+      // if (found.length > 0) {
+      //   user = name
+      //   loginPage.classList.add('hidden');
+      //   dashBoard.classList.remove('hidden');
+      // }
+    })
+    .catch(err => console.log(err));
 }
 
 
 
-
-
+checkForBookings(user.id)
+checkForRooms(user.id)
