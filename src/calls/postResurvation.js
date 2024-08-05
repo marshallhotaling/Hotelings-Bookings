028adc7021bec1a66241d21f75bc9b4a044b60b1
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-08-05 15:55:30 -0600

    refactord the post
    made the post abled to be called apon befor loding the dom

---
+++ src/calls/postResurvation.js
@@ -0,0 +1,18 @@
+export const postData = (data) => {
+  console.log('data', data)
+  return fetch(`http://localhost:3001/api/v1/bookings`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => {
+      if (response.status !== 201) {
+        throw new Error(`POST ERROR: Resource not created >>> STATUS ${response.status} - ${response.statusText}`)
+      }
+      return response.json()
+    })
+    .then(data => data)
+    .catch(error => alert(`POST ERROR >>> STATUS ${error.status} - ${error.statusText}`))
+}
